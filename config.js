7abc4d5ac7a514dd3814b455c3ac70b71bcb3b7e
Kayla Webb <kaylarwebb@gmail.com>
2017-11-29 11:50:51 -0500

    installed dependencies.

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
+'use strict';
+
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/blog-app';
+
+exports.TEST_DATABASE_URL = process.env.DATABASE_URL ||
+                            'mongodb://localhost/starter-test-blog-db';
+
+
 exports.PORT = process.env.PORT || 8080;
